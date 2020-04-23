e43a6f9f09894c0d8e6f9fdf140619c75b72066b
cyonsoft <48652415+cyonsoft@users.noreply.github.com>
2020-04-23 09:18:19 +0800

    Add files via upload
    This program helps to output 'big digits' of the input number.

---
+++ 01_01_bigdigits_myself.py
@@ -0,0 +1,89 @@
+import sys
+
+Zero = ["  ***  ",
+        " *   * ",
+        " *   * ",
+        " *   * ",
+        " *   * ",
+        " *   * ",
+        "  ***  "]
+One = ["  *  ",
+       " **  ",
+       "  *  ",
+       "  *  ",
+       "  *  ",
+       "  *  ",
+       " *** "]
+Two = ["  ***  ",
+       " *   * ",
+       " *  *  ",
+       "   *   ",
+       "  *    ",
+       " *     ",
+       " ***** "]
+Three = ["  ***  ",
+         " *   * ",
+         "     * ",
+         "   **  ",
+         "     *" ,
+         " *   * ",
+         "  ***  "]
+Four = ["     *  ",
+        "    **  ",
+        "   * *  ",
+        "  *  *  ",
+        " ****** ",
+        "     *  ",
+        "     *  "]
+Five = [" ***** ",
+        " *     ",
+        " *     ",
+        "  ***  ",
+        "     * ",
+        " *   * ",
+        "  ***  "]
+Six = ["  ***  ",
+       " *   * ",
+       " *     ",
+       " ****  ",
+       " *   * ",
+       " *   * ",
+       "  ***  "]
+Seven = [" ***** ",
+         "     * ",
+         "    *  ",
+         "   *   ",
+         "  *    ",
+         " *     ",
+         " *     "]
+Eight = ["  ***  ",
+         " *   * ",
+         " *   * ",
+         "  ***  ",
+         " *   * ",
+         " *   * ",
+         "  ***  "]
+Nine = ["  **** ",
+        " *   * ",
+        " *   * ",
+        "  **** ",
+        "     * ",
+        "     * ",
+        "  ***  "]
+number = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+
+original_number = input('input your integer')
+line = 0
+
+try:
+    while line < 7:
+        printing_number = ''
+        for each_number in original_number:
+            number_location = int(each_number)
+            printing_number += number[number_location][line]
+            printing_number = printing_number.replace('*',each_number) + ' '
+        print(printing_number)
+        line += 1
+
+except ValueError as err:
+    print(err)
